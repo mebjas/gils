3b870075f9438fa87cbce4ac6776a9b969ba3350
minhaz <minhazav@gmail.com>
2016-07-30 23:59:30 +0530

    Added certain functionalities to token store service
    - added api to register a worker
    - imporved api to register a boss
    - added api to add new token to system, and report it to boss

--- token-pool/index.js
+++ token-pool/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var sprintf = require('sprintf').sprintf;
+var request = require('request');
 var tokens = require('./tokens');
+
 
 var app = express();
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -9,17 +12,24 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 
 // DS to store all discovery information
-var discovery = function() {
-    this.boss = {};
-    this.workers = [];
-}
+/* REGION: Discovery class */{
+    var discovery = function() {
+        this.boss = {};
+        this.workers = [];
+    }
 
-discovery.prototype.registerBoss = function (req) {
-    throw 'TBD Exception';
-}
+    discovery.prototype.registerBoss = function (boss) {
+        this.boss = boss;
+        this.workers = [];
+        console.log('BOSS Registered: ' +boss.ip +':' +boss.port);
+        console.log('ALL Workers data cleared; WORKERS: ', this.workers);
+    }
 
-discovery.prototype.registerWorkers = function (req) {
-    throw 'TBD Exception';
+    discovery.prototype.registerWorker = function (worker) {
+        this.workers.push(worker);
+        console.log("WORKER ADDED TO SYSTEM", worker);
+        console.log('Worker Registered on port:', worker.port);
+    }
 }
 var DS = new discovery();
 
@@ -37,19 +47,60 @@ app.get('/get', function(req, res) {
  * Request to add a new token
  */
 app.post('/new', function(req, res) {
-   throw 'TBD Exception';
+    var t = req.body.token;
+    var l = req.body.login;
+    console.log(sprintf("new token request with token: %s, login: %s", t, l));
+
+    var token =  {
+        token: t,
+        login: l,
+        remaining: 0,
+        last_request: -1,
+        next: 0
+    };
+
+    tokens.push(token);
+    // send the message downstream
+
+    // TODO: remove protocol harcoding    
+    var bossAddr = sprintf("http://%s:%s/", DS.boss.ip, DS.boss.port);
+    request.post(bossAddr +'/new', {form: {token: token}}, function (err, httpResponse, body) {
+        if (err) {
+            console.log('[error] Unable to feed new token information to boss');
+            return;
+        }
+        console.log('Informed Boss about new token');
+        res.json({error: false, message: 'new token added successfully'});
+    });
 });
 
 /**
- * Request to add a new token
+ * Request to register boss
  */
 app.post('/registerBoss', function(req, res) {
-    DS.boss = {
+    DS.registerBoss({
         ip : req.body.ip,
         port: req.body.port
-    }
-    console.log('BOSS Registered: ' +req.body.ip +':' +req.body.port);
+    });
     res.json({error: false, message: 'boss registeration successfull'});
+});
+
+/**
+ * Request to register workers
+ */
+app.post('/registerWorker', function(req, res) {
+    // TODO: validate the params
+    var worker = {
+        host: "http://localhost", // TODO: remove this localhost hardcoding
+        port: req.body.port,
+        getAddr: function() {
+            return this.host +':' +this.port +'/';
+        },
+        token: req.body.token
+    }
+
+    DS.registerWorker(worker);
+    res.json({error: false, message: 'worker registeration successfull'});
 });
 
 
