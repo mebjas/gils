4a445611cd304f97d090f84982b46f7ec7a01e21
minhaz <minhazav@gmail.com>
2016-08-14 19:21:45 +0530

    Code to filter dataset to single <issue, labels[]> tuple array

---
+++ filter-dataset/index.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+var start = new Date().getTime();
+var data = [];
+var labelFreq = {};
+var lastUpdate = start;
+
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            // skip
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
+var files = getFiles('../dataset');
+files.forEach(function(_file) {
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(_file)
+    });
+
+    lineReader.on('line', function (line) {
+        try {
+            var d = JSON.parse(line);
+            if (!d.labels.length) return;
+            var entry = {issue: d.issue, labels: []};
+            d.labels.forEach(function(label) {
+                entry.labels.push(label.name);
+                console.log(label.name);
+
+                if (typeof labelFreq[label.name] == 'undefined')
+                    labelFreq[label.name] = 1;
+                else labelFreq[label.name] = labelFreq[label.name] + 1;
+            });
+
+            lastUpdate = new Date().getTime();
+            data.push(entry);
+        } catch (ex) {
+            console.log(ex);
+        }
+    });
+});
+
+
+function checkForLastUpdateAndLog() {
+    // diff by 5 seconds
+    if (new Date().getTime() > lastUpdate + 5000) {
+        // finish
+        console.log(labelFreq);
+        fs.writeFileSync('filter-issue-label.json', JSON.stringify(data));
+    } else {
+        console.log('current time: ', new Date().getTime());
+        console.log('lastUpdate time: ', lastUpdate);
+        console.log('Still working, no of entries = ', data.length);
+        setTimeout(checkForLastUpdateAndLog, 5000);
+    }
+}
+
+checkForLastUpdateAndLog();
